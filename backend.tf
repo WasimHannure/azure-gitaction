261eefb31cd3cb3f162dd788d506dffbb4ea89d6
Mr.Professor <wasim.hannure@yahoo.in>
2026-05-23 20:29:09 +0530

    webhook trigger

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "newrg"                       
+    resource_group_name  = "jenkins"                       
     storage_account_name = "mybackendbucket"                                 
     container_name       = "mycontainer"                                  
     key                  = "demo.terraform.tfstate"                   
